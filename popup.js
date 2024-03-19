c53863501ff0dc18eb66028f725500db5f6088c4
spinoandraptos <spinoandraptos@gmail.com>
2024-03-19 22:23:43 +0800

    Removed commented out sections

--- popup.js
+++ popup.js
@@ -5,22 +5,6 @@ var analysisList = [];
 chrome.runtime.sendMessage({data:"Handshake"},function(response){
 });
 
-/*
-Function that handles latest messages from background.js
-*/
-// chrome.runtime.onMessage.addListener(function(message,sender,sendResponse){
-//     messageList = message.messages;
-//     var str = '';
-//     if (messageList == []){
-//         str += '<div class="loader"></div>';
-//     } else {
-//         messageList.forEach(function(message) {
-//             str += '<div id="emailContent">' + '<p>' + message + '</p>' + '</div>';
-//         });       
-//     }
-//     document.getElementById("messages").innerHTML = str;
-// });
-
 chrome.runtime.onMessage.addListener(function(message,sender,sendResponse){
     senderList = message.senders;
     topicList = message.topics;
